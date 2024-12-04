c5d6d6d2bc16c3de54cdf688891982adc3cddbd3
Nyamadi Atsu <devbrainy98@gmail.com>
2024-12-04 21:57:27 +0100

    update: vercel.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://formbuilder-super.vercel.app"],
+  })
+);
 app.use(express.json());
 
 mongoose.connect(
@@ -17,6 +21,10 @@ mongoose.connect(
 );
 
 app.use("/api/forms", formRoutes);
+
+app.use("*", (req, res) => {
+  res.status(200).json({ message: "API working fine", success: false });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
